490ae027140c2176e0b2b83aaa380926c3528b35
BeRobi <screengsm@gmail.com>
2023-11-25 12:12:48 +0100

    validalas

--- View/TextUrlapView.js
+++ View/TextUrlapView.js
@@ -1,19 +1,33 @@
-export default class TextUrlapView{
-    constructor(szuloElem, obj, key){
-            this.szuloElem = szuloElem;
-            this.obj=obj;
-            this.key=key;
-            this.textUrlapElem();
-            this.inputElem=$(`#${this.key}`)
-            
-    }
+export default class TextUrlapView {
+  #value = "";
+  #valid = false;
 
-    getValue(){
-            return this.inputElem.val()
-    }
+  constructor(szuloElem, obj, key) {
+    this.szuloElem = szuloElem;
+    this.obj = obj;
+    this.key = key;
+    this.textUrlapElem();
+    this.inputElem = $(`#${this.key}`);
+    this.inputElem.on("keyup", () => {
+      console.log(this.inputElem.val());
+      this.#value = this.inputElem.val();
+      //valid-e?
+      let patternString = this.obj.pattern; // reguláris kifejezés
+      let pattern = new RegExp(patternString); //reguláris kifejezés átalakítása reguláris objektummá
+      if (pattern.test(this.#value)) {
+        this.#valid = true;
+      } else this.#valid = false;
 
-    textUrlapElem(){
-        let txt = `<div class="mb-3 mt-3">
+      console.log(this.#valid);
+    });
+  }
+
+  getValue() {
+    return this.inputElem.val();
+  }
+
+  textUrlapElem() {
+    let txt = `<div class="mb-3 mt-3">
          <label for="${this.key}" class="form-label">${this.obj.megjelenes}</label>
          <input type="${this.obj.tipus}" class="form-control" 
              id="${this.key}" 
@@ -21,8 +35,7 @@ export default class TextUrlapView{
              pattern="${this.obj.pattern}"
              value="${this.obj.value}"
              name="${this.key}">
-         </div>`  
-        this.szuloElem.append(txt)
-       }
-
+         </div>`;
+    this.szuloElem.append(txt);
+  }
 }
